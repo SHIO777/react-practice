da387f3b83f2a9f49a570240d098a45b2228f0ce
SHIO777 <shiotatsu1015@gmail.com>
2023-09-21 16:06:32 +0900

    update: add clickボタン

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { useState } from "react";
 import ColorList from "./6/ColorList";
 import colorData from "./6/color-data.json";
+import AddColorForm from "./6/AddColorForm";
+import { v4 } from "uuid";
 
 // 6.1
 // export default function App() {
@@ -22,21 +24,99 @@ import colorData from "./6/color-data.json";
 // }
 
 // 6.2.2
+// export default function App() {
+//   const [colors, setColors] = useState(colorData);
+//   return (
+//     <ColorList
+//       colors={colors}
+//       onRateColor={(id, rating) => {
+//         const newColors = colors.map(color =>
+//           color.id === id ? { ...color, rating } : color
+//         );
+//         setColors(newColors);
+//       }}
+//       onRemoveColor={id => {
+//         const newColors = colors.filter(color => color.id !== id);
+//         setColors(newColors);
+//       }}
+//     />
+//   );
+// }
+
+// 6.3
+
+// export default function App() {
+//   const [colors, setColors] = useState(colorData);
+
+//   const removeColor = id => {
+//     const newColors = colors.filter(color => color.id !== id);
+//     setColors(newColors);
+//   };
+
+//   const rateColor = (id, rating) => {
+//     const newColors = colors.map(color =>
+//       color.id === id ? { ...color, rating } : color
+//     );
+//     setColors(newColors);
+//   };
+
+//   return (
+//     <>
+//       <AddColorForm
+//         onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
+//       />
+//       <ColorList
+//         colors={colors}
+//         onRemoveColor={removeColor}
+//         onRateColor={rateColor}
+//       />
+//     </>
+//   );
+// }
+
+// 6.3.4
 export default function App() {
-    const [colors, setColors] = useState(colorData);
+  const [colors, setColors] = useState(colorData);
+
+  const removeColor = id => {
+    const newColors = colors.filter(color => color.id !== id);
+    setColors(newColors);
+  };
+
+  const rateColor = (id, rating) => {
+    const newColors = colors.map(color =>
+      color.id === id ? { ...color, rating } : color
+    );
+    setColors(newColors);
+  };
+
+  const createColor = (title, color) => {
+    const newColors = [
+      ...colors,
+      {
+        id: v4(),
+        rating: 0,
+        title,
+        color
+      }
+    ];
+    setColors(newColors);
+  };
+
+  const [update, setUpdate] = useState(false);
+
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map(color =>
-          color.id === id ? { ...color, rating } : color
-        );
-        setColors(newColors);
-      }}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors);
-      }}
-    />
+    <>
+      <button onClick={() => setUpdate((prevState) => !prevState)}>
+        {update ? 'update' : '!update'}
+      </button>
+      
+      <AddColorForm onNewColor={createColor} />
+      <ColorList
+        colors={colors}
+        onRemoveColor={removeColor}
+        onRateColor={rateColor}
+      />
+    </>
   );
 }

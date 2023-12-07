4a08e9ab5577fe792c8ede11fe15f020dad961bc
SHIO777 <shiotatsu1015@gmail.com>
2023-12-08 03:49:41 +0900

    update

--- src/6/SearchApp.js
+++ src/6/SearchApp.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react"
+import React, {useState, useRef, useEffect} from "react"
 
 const SearchApp = () => {
     const [input, setInput] = useState("")
     const [hits, setHits] = useState(0)
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [highlightIndex, setHighlightIndex] = useState(0)
+    const containerRef = useRef(null);
 
     const data = [
         "Hello my name is is",
@@ -25,7 +25,82 @@ const SearchApp = () => {
         "b",
         "c",
         "hello Hello",
+        "Hello my name is is",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        "a",
+        "b",
+        "c",
+        "hello Hello",
+        "Hello my name is is",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        "a",
+        "b",
+        "c",
+        "hello Hello",
+        "Hello my name is is",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        "a",
+        "b",
+        "c",
+        "hello Hello",
     ];
+
+    // const data = [
+    //     "Hello my name is is",
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    //     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    //     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    //             'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    //     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    //             'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    //     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    //             'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    //     'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    //     'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    //     'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    //     'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //     "a",
+    //     "b",
+    //     "c",
+    //     "hello Hello",
+    // ];
 
     const handleInputChange = (e) => {
         const inputValue = e.target.value
@@ -48,71 +123,50 @@ const SearchApp = () => {
             })
         })
         setHits(hitsNum)
-        // setCurrentIndex(0)
     }
 
-    // const highlightText = (text, query) => {
-    //     let highlightNum = 0;
-    //     if (!query) return text;
-    //     const parts = text.split(new RegExp(`(${query})`, "gi"))
-    //     console.log(parts)
 
-    //     return parts.map((part, index) => {
-    //         const isHighlightWord = part.toLowerCase() === query.toLowerCase();
-    //         const highlight = highlightIndex
 
-    //         if (isHighlightWord) {
-    //             setHighlightIndex(highlightIndex+1)
-    //         }
-
-    //         return isHighlightWord ? (
-    //             <mark key={index} style={{ background: (highlight) === currentIndex ? "orange" : "yellow" }}>
-    //             {part}
-    //         </mark>
-    //         ) : (
-    //         part
-    //         );
-    //     });
-
+    var highlightNum = 0;
 
     const highlightText = (text, query) => {
-        let highlightNum = 0;
         if (!query) return text;
         const parts = text.split(new RegExp(`(${query})`, "gi"))
-        console.log(parts)
 
         return parts.map((part, index) => {
             const isHighlightWord = part.toLowerCase() === query.toLowerCase();
-            const highlight = highlightIndex
-
             if (isHighlightWord) {
                 highlightNum += 1;
-                // setHighlightIndex(highlightIndex+1)
+            }
+
+            if (isHighlightWord && highlightNum - 1 === currentIndex) {
+                // スクロール
+                // containerRef.current &&
+                // containerRef.current.scrollTo({
+                //     top: containerRef.current.scrollTop + containerRef.current.querySelector(`#word-${index}`).offsetTop,
+                //     behavior: "smooth",
+                // });
+                if (containerRef.current) {
+                    const targetWord = containerRef.current.querySelector(`#word-${currentIndex}`);
+                    if (targetWord) {
+                        containerRef.current.scrollTo({
+                            top: containerRef.current.scrollTop + targetWord.offsetTop,
+                            behavior: "smooth",
+                        });
+                    }
+                }
             }
 
             return isHighlightWord ? (
-                // <mark key={index} style={{ background: (highlight) === currentIndex ? "orange" : "yellow" }}>
-                <mark key={index} style={{ background: (highlightNum) === currentIndex ? "orange" : "yellow" }}>
-                {/* {(highlight)} */}
+                <mark key={index} id={`word-${index}`} style={{ background: (highlightNum-1) === currentIndex ? "orange" : "yellow" }}>
+                {/* {currentIndex},
+                {highlightNum} */}
                 {part}
             </mark>
             ) : (
             part
             );
         });
-
-
-        // return parts.map((part, index) =>
-        //     part.toLowerCase() === query.toLowerCase() ? (
-        //         // <mark key={index}>{part}</mark>
-        //         // <mark key={index} style={{ color: index === currentIndex ? "orange" : "inherit" }}>
-        //         <mark key={index} style={{ background: index === currentIndex ? "orange" : "yellow" }}>
-        //             {part}
-        //         </mark>
-        //     ) : (
-        //             part
-        //     )
-        // )
     }
 
     const handleArrowClick = (direction) => {
@@ -126,6 +180,19 @@ const SearchApp = () => {
             }
         }
     }
+
+    useEffect(() => {
+        // コンポーネントがマウントされた後に操作を行う
+        if (containerRef.current) {
+            const targetWord = containerRef.current.querySelector(`#word-${currentIndex}`);
+            if (targetWord) {
+                containerRef.current.scrollTo({
+                    top: containerRef.current.scrollTop + targetWord.offsetTop,
+                    behavior: "smooth",
+                });
+            }
+        }
+    }, [currentIndex]);
 
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
@@ -146,11 +213,17 @@ const SearchApp = () => {
                 </p>
                 )}
             </div>
-            <div>
+            <div ref={containerRef} style={{ overflowY: "scroll", maxHeight: "500px" }}>
+            {/* <div ref={containerRef} style={{ overflowY: "scroll" }}> */}
                 {data.map((text, index) => (
                 <div key={index}>{highlightText(text, input)}</div>
                 ))}
             </div>
+            {/* <div>
+                {data.map((text, index) => (
+                <div key={index}>{highlightText(text, input)}</div>
+                ))}
+            </div> */}
         </div> 
     )
 }
